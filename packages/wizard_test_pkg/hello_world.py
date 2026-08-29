be5500d7020a3c395f3e77bd1eeb1e592805cad8
AdamBelloum <a.s.z.belloum@uva.nl>
2026-08-29 23:45:15 +0200

    Remove package wizard_test_pkg

--- packages/wizard_test_pkg/hello_world.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import yaml
-
-def hello_world():
-    result= {"ouptu": "hello formpython!"}
-    println(ymal.dump(result, default_flow_style=True).strip())
-
-if __name__ ="__main__"
-    main()
--- packages/wizard_test_pkg/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-test -f container.yml
-test -f hello_world.sh
-grep -qx 'name: wizard_test_pkg' container.yml
-
-echo "Package structure checks passed."
